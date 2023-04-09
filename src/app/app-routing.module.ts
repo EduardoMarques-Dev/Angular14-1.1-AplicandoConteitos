57b6f51fdaa19760865ac34f23ff677f04483e55
Carlos Pereira <carlos.pereira@spcm.com.br>
2023-04-09 00:30:41 -0300

    Aula 03.~~ Navegação com roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { CriarPensamnetoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar-pensamento',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamnetoComponent
+  },
+  {
+    path: 'listar-pensamento',
+    component: ListarPensamentoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
